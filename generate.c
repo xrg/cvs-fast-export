0d2c21f569d09bdf40a27f1ca635f9d8ff9e7ed2
Eric S. Raymond <esr@thyrsus.com>
2015-11-21 16:19:03 -0500

    Replace GPL with SPDX tag.

--- generate.c
+++ generate.c
@@ -2,19 +2,7 @@
  * Copyright © 2006 Sean Estabrooks <seanlkml@sympatico.ca>
  * Copyright © 2006 Keith Packard <keithp@keithp.com>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or (at
- *  your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  SPDX-License-Identifier: GPL-2.0+
  *
  * Large portions of code contained in this file were obtained from
  * the original RCS application under GPLv2 or later license, it retains
